5de4afbf7528a524f6a71d4b80779dc2dff58999
koraonlinefans <57924819+koraonlinefans@users.noreply.github.com>
2019-11-26 19:41:12 +0200

    Add files via upload

---
+++ t.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    function prepareFrame() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://vk.com/video_ext.php?oid=550388570&id=456239029&hash=dc4aa9c441fd2d1f");
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+    }
+</script>
